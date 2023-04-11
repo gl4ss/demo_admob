09bbd1126821ef2d8bad517e5cd65e8965b2423a
DucAnhPotato <89984470+DucAnhPotato@users.noreply.github.com>
2023-04-12 00:32:08 +0700

    commit change

--- app/src/main/java/edu/hanu/demo_admob/MainActivity.java
+++ app/src/main/java/edu/hanu/demo_admob/MainActivity.java
@@ -3,6 +3,14 @@ package edu.hanu.demo_admob;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.ads.nativetemplates.NativeTemplateStyle;
+import com.google.android.ads.nativetemplates.TemplateView;
+import com.google.android.gms.ads.AdLoader;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.nativead.NativeAd;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button adButton = findViewById(R.id.adButton);
+        adButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Native Ads
+                AdLoader adLoader = new AdLoader.Builder(MainActivity.this, "ca-app-pub-3940256099942544/2247696110")
+                        .forNativeAd(new NativeAd.OnNativeAdLoadedListener() {
+                            @Override
+                            public void onNativeAdLoaded(NativeAd nativeAd) {
+                                NativeTemplateStyle styles = new
+                                        NativeTemplateStyle.Builder().withMainBackgroundColor(null).build();
+                                TemplateView template = findViewById(R.id.my_template);
+                                template.setVisibility(View.VISIBLE);
+                                template.setStyles(styles);
+                                template.setNativeAd(nativeAd);
+                            }
+                        })
+                        .build();
+
+                adLoader.loadAd(new AdRequest.Builder().build());
+            }
+        });
+
+
     }
 }
